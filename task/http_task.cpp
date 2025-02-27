2526305c1fa6529715af347f068cfc95ddd72530
jiafie <67643874+jiafie7@users.noreply.github.com>
2025-02-27 14:32:14 +0800

    commit large file transfer

--- task/http_task.cpp
+++ task/http_task.cpp
@@ -7,11 +7,29 @@ using namespace melon::socket;
 
 HttpTask::HttpTask(int socket_fd)
   : m_socket_fd(socket_fd)
+  , m_total_len(0)
+  , m_head_len(0)
+  , m_body_len(0)
+  , m_body_idx(0)
+  , m_body(nullptr)
 {
 }
 
 HttpTask::~HttpTask()
 {
+}
+
+void HttpTask::reset()
+{
+  m_total_len = 0;
+  m_head_len = 0;
+  m_body_len = 0;
+  m_body_idx = 0;
+  if (m_body != nullptr)
+  {
+    delete[] m_body;
+    m_body = nullptr;
+  }
 }
 
 void HttpTask::run()
@@ -20,7 +38,7 @@ void HttpTask::run()
   
   Socket socket(m_socket_fd);
 
-  char buf[1024] = {0};
+  char buf[recv_buff_size] = {0};
 
   int len = socket.recv(buf, sizeof(buf));
 
@@ -48,15 +66,47 @@ void HttpTask::run()
   }
   log_debug("recv: conn = %d, msg = %s", m_socket_fd, buf);
 
-  Request req;
-  int ret = req.parseHeader(buf, len);
-  req.parseBody(buf + ret, len - ret);
-  req.show(); //  log request parse results
+  // m_total_len accumulate the length of data received each request
+  m_total_len += len;
 
-  Server* server = Singleton<Server>::getInstance();
-  std::string resp = server->handle(req);
+  // if m_head_len is 0, it means that this is the first request coming from this connection
+  if (m_head_len == 0)
+  {
+    // start parse http request headers
+    m_head_len = m_req.parseHeader(buf, len);
+    m_body_len = m_req.contentLength();
 
-  socket.send(resp.c_str(), resp.size());
+    // if m_body_len isn't 0, means that this request has a request body
+    if (m_body_len > 0)
+    {
+      m_body = new char[m_body_len + 1];
+      m_body[m_body_len] = 0;
+      // extract request body data
+      std::memcpy(m_body, buf + m_head_len, len - m_head_len);
+      m_body_idx = len - m_head_len;
+    }
+  }
+  else // not first request, all request body data
+  {
+    std::memcpy(m_body + m_body_idx, buf, len);
+    m_body_idx += len;
+  }
+
+  // the request data of the current connection has been received end
+  if (m_total_len >= m_head_len + m_body_len)
+  {
+    // start parse http request body
+    m_req.parseBody(m_body, m_body_len);
+  
+    // handle request, generate http response
+    Server* server = Singleton<Server>::getInstance();
+    std::string resp = server->handle(m_req);
+    // send http response to client
+    socket.send(resp.c_str(), resp.size());
+
+    // current request handle completed, reset all variables, waiting for next request
+    reset();
+  }
 }
 
 void HttpTask::destroy()
--- task/http_task.h
+++ task/http_task.h
@@ -15,19 +15,28 @@ namespace melon
 {
   namespace task
   {
+    const uint32_t recv_buff_size = 1024 * 8; // 8 KB
     class HttpTask : public Task
     {
     public:
       HttpTask() = delete;
       HttpTask(int socket_fd);
       ~HttpTask();
-        
+
+      void reset();
       void run();
       void destroy();
 
     private:
       int m_socket_fd = 0;
       bool m_closed = false;
+
+      Request m_req;
+      int m_total_len;
+      int m_head_len;
+      int m_body_len;
+      int m_body_idx; // next continue location for uploading
+      char* m_body;   // data recv buffer
     };
   }
 }
